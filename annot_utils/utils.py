ecf5d7ae87394d20e99ac38c5cbf3b94cd33b953
friend1ws <friend1ws@gmail.com>
2019-09-06 03:04:34 +0000

    update the gene model files

--- annot_utils/utils.py
+++ annot_utils/utils.py
@@ -11,17 +11,20 @@ def set_ucsc_gene_file(genome_id, gene_model):
         if gene_model == "refseq":
             ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg19/refGene.txt.gz")
         elif gene_model == "gencode":
-            ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg19/wgEncodeGencodeBasicV19.txt.gz")
+            # ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg19/wgEncodeGencodeBasicV19.txt.gz")
+            ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg19/wgEncodeGencodeCompV31lift37.txt.gz")
     elif genome_id == "hg38":
         if gene_model == "refseq":
             ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg38/refGene.txt.gz")
         elif gene_model == "gencode":
-            ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg38/wgEncodeGencodeBasicV24.txt.gz")
+            # ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg38/wgEncodeGencodeBasicV24.txt.gz")
+            ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/hg38/wgEncodeGencodeCompV31.txt.gz")
     elif genome_id == "mm10":
         if gene_model == "refseq":
             ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/mm10/refGene.txt.gz")
         elif gene_model == "gencode":
-            ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/mm10/wgEncodeGencodeBasicVM9.txt.gz")
+            # ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/mm10/wgEncodeGencodeBasicVM9.txt.gz")
+            ucsc_gene_file = pkg_resources.resource_filename("annot_utils", "data/mm10/wgEncodeGencodeCompVM22.txt.gz")
     else:
         logger.error("genome_id shoud be hg19, hg38 or mm10.")
         sys.exit(1)
--- resource/prep_data.sh
+++ resource/prep_data.sh
@@ -20,22 +20,27 @@ fi
 
 # for GRCh37 (hg19)
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz
-wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/wgEncodeGencodeBasicV19.txt.gz 
+# wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/wgEncodeGencodeBasicV19.txt.gz 
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/wgEncodeGencodeCompV31lift37.txt.gz
 mv refGene.txt.gz ../annot_utils/data/hg19
-mv wgEncodeGencodeBasicV19.txt.gz ../annot_utils/data/hg19
+# mv wgEncodeGencodeBasicV19.txt.gz ../annot_utils/data/hg19
+mv wgEncodeGencodeCompV31lift37.txt.gz ../annot_utils/data/hg19
 
 # for GRCh38 (hg38)
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz
-wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV28.txt.gz
+# wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV28.txt.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeCompV31.txt.gz 
 mv refGene.txt.gz ../annot_utils/data/hg38
-mv wgEncodeGencodeBasicV28.txt.gz ../annot_utils/data/hg38
+# mv wgEncodeGencodeBasicV28.txt.gz ../annot_utils/data/hg38
+mv wgEncodeGencodeCompV31.txt.gz ../annot_utils/data/hg38
 
 # for GRCm38 (mm10)
 wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/database/refGene.txt.gz
-wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/database/wgEncodeGencodeBasicVM18.txt.gz  
+# wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/database/wgEncodeGencodeBasicVM18.txt.gz  
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/database/wgEncodeGencodeCompVM22.txt.gz
 mv refGene.txt.gz ../annot_utils/data/mm10
-mv wgEncodeGencodeBasicVM18.txt.gz ../annot_utils/data/mm10
-
+# mv wgEncodeGencodeBasicVM18.txt.gz ../annot_utils/data/mm10
+mv wgEncodeGencodeCompVM22.txt.gz ../annot_utils/data/mm10
 
 
 
